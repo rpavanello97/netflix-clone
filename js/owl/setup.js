f31b46b7cba1a939a8dd251e60f0e1d667b09834
rpavanello97 <rpavanello97@hotmail.com>
2021-09-10 13:21:31 -0300

    Commit all page

---
+++ js/owl/setup.js
@@ -0,0 +1,27 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:6
+        }
+    }
+});
